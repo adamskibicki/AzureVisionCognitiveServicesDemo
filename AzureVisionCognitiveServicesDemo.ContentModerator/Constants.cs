524ef1e313bd0e2aebba879cfec8cdff30906fae
adamskibicki <adam-skibicki@outlook.com>
2019-02-25 20:40:36 +0100

    Add initial demo for content moderator

---
+++ AzureVisionCognitiveServicesDemo.ContentModerator/Constants.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AzureVisionCognitiveServicesDemo.ContentModerator
+{
+    public static class Constants
+    {
+        public const string SUBSCRIPTION_KEY = "Your_computer_vision_key";
+
+        public const string ENDPOINT_URL = "https://westeurope.api.cognitive.microsoft.com";
+    }
+}
---
+++ AzureVisionCognitiveServicesDemo.ContentModerator/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.Azure.CognitiveServices.ContentModerator;
+
+namespace AzureVisionCognitiveServicesDemo.ContentModerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContentModeratorClient contentModeratorClient = new ContentModeratorClient(new ApiKeyServiceClientCredentials(Constants.SUBSCRIPTION_KEY));
+
+            contentModeratorClient.Endpoint = Constants.ENDPOINT_URL;
+        }
+    }
+}
